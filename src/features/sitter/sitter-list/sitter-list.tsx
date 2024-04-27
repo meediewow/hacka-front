aa7e6ef94bba5d339b8183c7079a6bfb96504c88
seryozha_now <baleyko.s@promail366.com>
2024-04-27 14:10:48 +0300

    Add key to map function in SitterList component
    The code has been updated to include a unique 'key' in the map function of the 'SitterList' component. This key helps React identify which items have changed, are added, or are removed, improving performance for larger lists.

--- src/features/sitter/sitter-list/sitter-list.tsx
+++ src/features/sitter/sitter-list/sitter-list.tsx
@@ -11,8 +11,10 @@ const sitters = Array.from({ length: 10 }).map((_, index) => ({
 export const SitterList: React.FC<SitterListProps> = () => {
     return (
         <Stack spacing={0.5} p={1}>
-            {sitters.map((sitter) => (
-                <Paper sx={{ p: 1 }}>{sitter.id}</Paper>
+            {sitters.map((sitter, index) => (
+                <Paper key={index} sx={{ p: 1 }}>
+                    {sitter.id}
+                </Paper>
             ))}
         </Stack>
     );
