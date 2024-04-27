6c5c8524fb4f4e12e68a6d2499a5430a9a582f2b
seryozha_now <baleyko.s@promail366.com>
2024-04-27 20:29:14 +0300

    Improve SitterProfile component UI
    The sitter-profile.tsx file was revised to enhance the user interface. Box, Stack, Rating, and Typography components were imported from MUI framework to organize content in a more structured and readable way, including text details of the sitter. Additionally, Avatar styling was revised for better alignment and presentation.

--- src/features/sitter/sitter-profile/sitter-profile.tsx
+++ src/features/sitter/sitter-profile/sitter-profile.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import { Accessory } from '@/shared/ui/accessory';
 import { ContentCard } from '@/shared/ui/content-card';
 import type { SitterProfileProps } from './types';
-import Box from '@mui/material/Box';
 
 export const SitterProfile: React.FC<SitterProfileProps> = () => {
     return (
         <ContentCard>
-            <Box display="flex" flexDirection="column">
-                <Avatar
-                    src="https://mui.com/static/images/avatar/1.jpg"
-                    alt=""
-                    sx={{ width: 128, height: 128 }}
-                />
-            </Box>
+            <Stack spacing={1}>
+                <Box display="flex" alignItems="center" justifyContent="center">
+                    <Avatar
+                        src="https://mui.com/static/images/avatar/1.jpg"
+                        alt=""
+                        sx={{ width: 128, height: 128 }}
+                    />
+                </Box>
+                <Box display="flex" flexDirection="column" alignItems="center">
+                    <Typography variant="h6">Наталья Александрова</Typography>
+                    <Typography color="text.secondary">
+                        Agiou Spiridonos 68, Limassol 3025
+                    </Typography>
+                </Box>
+                <Box
+                    display="flex"
+                    gap={0.75}
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Box display="flex" gap={0.5} alignItems="center">
+                        <Rating defaultValue={1} max={1} size="small" readOnly />
+                        <Typography>4,9</Typography>
+                    </Box>
+                    <Accessory />
+                    <Typography color="success.main">29 повторных заказов</Typography>
+                </Box>
+            </Stack>
         </ContentCard>
     );
 };
