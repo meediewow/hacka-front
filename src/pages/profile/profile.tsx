a9f00bafa2590f8d2c24c09df98dbdaab801de98
litovskikh.o <oleg.litovski9@gmail.com>
2024-04-28 15:26:12 +0300

    logput

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Stack from '@mui/material/Stack';
-import { useUser } from '@/features/auth';
+import { AuthContext, useUser } from '@/features/auth';
 import { UserProfile } from '@/features/user/user-profile';
 import { TariffEditedBox } from '@/features/tariff/tariff-edited-box/tariff-edited-box';
 import { PetEditedBox } from '@/features/pet/pet-edited-box/pet-edited-box';
 import { OrderList } from '@/features/orders/order-list/order-list';
+import Button from '@mui/material/Button';
 
 export const ProfilePage: React.FC = () => {
     const user = useUser();
+
+    const { setToken } = useContext(AuthContext);
 
     if (!user) {
         return null;
     }
 
+    const logOut = () => {
+        setToken('');
+
+        window.location.href = '/';
+    };
+
     return (
         <Stack width="100%" p={1} justifyContent="center" gap={0.5}>
             <UserProfile user={user} />
+
+            <Button variant="outlined" color="primary" onClick={() => logOut()}>
+                Выйти
+            </Button>
 
             {user.isSitter && <TariffEditedBox initialTariffs={user.tariff ?? []} />}
 
