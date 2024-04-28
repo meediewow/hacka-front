04d0c2f2fc3749a35bf7c271bb9d9156590d502a
litovskikh.o <oleg.litovski9@gmail.com>
2024-04-28 11:16:07 +0300

    update sitters

--- src/entities/sitter/api/get-sitters.ts
+++ src/entities/sitter/api/get-sitters.ts
@@ -1,5 +1,6 @@
 import { api } from '@/shared/api';
 import type { SittersVariables } from '@/entities/sitter/types';
+import { UserApiModel } from '@/entities/user/types';
 
 export const getSitters = async (
     data: SittersVariables
